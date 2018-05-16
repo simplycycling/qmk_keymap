c247682f8e3fe82e4269124fea6a190c085f84d5
Roger Sherman <roger.sherman@quantium.com.au>
2018-05-16 16:43:33 +1000

    Added an alt/insert (Jetbrains specific) in place of the Hyper button

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           | Alt  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |Pscreen------+------+------+------+------+--------|
  * |   -    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Esc  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|AltIns|           | Esc  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |CtlShf| Alt  |  Cmd |                                       |  Up  | Down |   [  |   ]  | ~L1  |
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_MINS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_INSERT),
         LT(SYMB,KC_GRV),KC_QUOT,      LSFT(KC_LCTL),  KC_LALT,KC_LGUI,
                                               KC_LEFT,        KC_RIGHT,
                                                               KC_HOME,
